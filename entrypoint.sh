9457c62719d0247b44239dcf8f094526a59699fa
Serge Logvinov <serge.logvinov@sinextra.dev>
2023-01-25 19:29:35 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [[ -z $RUNNER_NAME ]]; then
     export RUNNER_NAME=`hostname`
 fi
 
-if [[ -z $RUNNER_REPOSITORY_URL ]]; then
+if [[ -z $RUNNER_REPOSITORY_URL && -z $RUNNER_ORGANIZATION_URL ]]; then
     echo "Error : You need to set the RUNNER_REPOSITORY_URL environment variable."
     exit 1
 fi
@@ -17,23 +17,34 @@ fi
 if [[ -f ".runner" ]]; then
     echo "Runner already configured. Skipping config."
 else
+    if [[ ! -z $RUNNER_ORGANIZATION_URL ]]; then
+        SCOPE="orgs"
+        RUNNER_URL="${RUNNER_ORGANIZATION_URL}"
+    else
+        SCOPE="repos"
+        RUNNER_URL="${RUNNER_REPOSITORY_URL}"
+    fi
+
     if [[ -z $RUNNER_TOKEN ]]; then
-        _PATH_="$(echo $RUNNER_REPOSITORY_URL | cut -d/ -f4-)"
+        _PATH_="$(echo $RUNNER_URL | cut -d/ -f4-)"
 
         RUNNER_TOKEN="$(curl -XPOST -fsSL \
             -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
             -H "Accept: application/vnd.github.v3+json" \
-            "https://api.github.com/repos/${_PATH_}/actions/runners/registration-token" \
+            "https://api.github.com/${SCOPE}/${_PATH_}/actions/runners/registration-token" \
             | jq -r '.token')"
     fi
 
     /app/config.sh \
-        --url $RUNNER_REPOSITORY_URL \
+        --url $RUNNER_URL \
         --token $RUNNER_TOKEN \
         --name $RUNNER_NAME \
         --work $RUNNER_WORK_FOLDER \
         --replace \
         --unattended
+
+    unset RUNNER_TOKEN
+    unset GITHUB_ACCESS_TOKEN
 fi
 
 exec /usr/sbin/immortal -w 15 -n -l /dev/stdout -d /app /app/bin/Runner.Listener run --startuptype service
